b7065e6bc61a276d889bdded2304aa9976c2bdc9
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2025-02-11 15:12:34 +0100

    Upgrade sign up and login logic

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF28458F);
-  static const Color secondaryColor = Color(0xFFF7DF00);
-  static const Color ternaryColor = Color(0xFF81819B);
-  static const Color grayFineColor = Color(0xFFECECEF);
-  static const Color blueBold = Color(0xFF011A41);
-  static const Color backColor = Color(0xFFF5F6FB);
-  static const Color grayDarkColor = Color(0xFF7C7C7C);
-  static const Color backColorFine = Color(0xFFDAE1EF);
-  static const Color udemColors = Color(0xFF2D2F31);
-  static const Color backWrite = Color(0xFF494D52);
-  static const Color fond = Color(0xFF8DACD3);  
+  static const Color primaryColor = Color(0xFF0a171e);
+  static const Color secondaryColor = Color(0xFF0a171e);
+
+  static const Color bottomBackColor = Color(0xFF050e12);
+  static const Color backgroundColor = Color(0xFF0a171e);
+  static const Color ternaryColor = Color(0xFF5d7a5d);
+  static const Color grayFineColor = Color(0xFF3d4648);
+  
+  // static const Color blueBold = Color(0xFF011A41);
+  // static const Color backColor = Color(0xFFF5F6FB);
+  // static const Color grayDarkColor = Color(0xFF7C7C7C);
+  // static const Color backColorFine = Color(0xFFDAE1EF);
+  // static const Color udemColors = Color(0xFF2D2F31);
+  // static const Color backWrite = Color(0xFF494D52);
+  // static const Color fond = Color(0xFF8DACD3);
 }
