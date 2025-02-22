0444023520b6fd3660ef74796b19de8e83808496
Dissang Tenkam Francis Kevin <dissangfrancis@yahoo.com>
2025-02-23 00:53:23 +0100

    update Message Bubble widget

--- lib/presentation/widget/message_bubble.dart
+++ lib/presentation/widget/message_bubble.dart
@@ -7,24 +7,24 @@ class MessageBubble extends StatefulWidget {
   final String text;
   final bool isMe;
   final String time;
-  final Function() onReply;
+  
   final Function() onCopy;
   final Function() onPin;
   final Function() onForward;
   final Function() onDelete;
-  final Function() onSelect;
+  
 
   const MessageBubble({
     super.key,
     required this.text,
     required this.isMe,
     required this.time,
-    required this.onReply,
+   
     required this.onCopy,
     required this.onPin,
     required this.onForward,
     required this.onDelete,
-    required this.onSelect,
+    
   });
 
   @override
@@ -33,6 +33,7 @@ class MessageBubble extends StatefulWidget {
 
 class _MessageBubbleState extends State<MessageBubble> {
   double _opacity = 0.0; // Initial opacity
+  final ScrollController _scrollController = ScrollController(); // Contrôleur de défilement
 
   @override
   void initState() {
@@ -51,158 +52,153 @@ class _MessageBubbleState extends State<MessageBubble> {
   void _showContextMenu(BuildContext context) {
     final RenderBox renderBox = context.findRenderObject() as RenderBox;
     final Offset offset = renderBox.localToGlobal(Offset.zero);
+    final double screenHeight = MediaQuery.of(context).size.height;
 
-    showDialog(
-      context: context,
-      barrierColor: Colors.transparent, // Fond transparent pour le flou
-      barrierDismissible: true, // Permettre la fermeture en cliquant en dehors
-      builder: (BuildContext context) {
-        return GestureDetector(
-          onTap: () {
-            // Fermer le menu contextuel lorsque l'utilisateur clique en dehors
-            Navigator.pop(context);
-          },
-          child: Stack(
-            children: [
-              // Effet de flou sur l'arrière-plan
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                child: Container(
-                  color: Colors.transparent,
-                ),
-              ),
-              // Positionnement du MessageBubble au-dessus du flou
-              Positioned(
-                top: offset.dy,
-                left: widget.isMe ? null : offset.dx, // Ajuster la position horizontale
-                right: widget.isMe ? offset.dx : null,
-                child: Material(
-                  color: Colors.transparent,
-                  child: Column(
-                    crossAxisAlignment:
-                        widget.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-                    children: [
-                      // Bulle de message
-                      BubbleSpecialThree(
-                        text: widget.text,
-                        isSender: widget.isMe,
-                        color: widget.isMe ? AppColors.primaryColor : Colors.grey[800]!,
-                        tail: true,
-                        textStyle: const TextStyle(
-                          fontSize: 14,
-                          color: Colors.white,
-                        ),
-                        sent: true,
-                        seen: true,
-                      ),
-                      // Horodatage
-                      Padding(
-                        padding: const EdgeInsets.only(top: 4),
-                        child: Text(
-                          widget.time,
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.7),
-                            fontSize: 10,
-                          ),
-                        ),
-                      ),
-                    ],
+    // Calculer si le menu doit s'afficher en dessous ou au-dessus du message
+    final bool showBelow = offset.dy + renderBox.size.height + 160 < screenHeight;
+
+    // Faire défiler automatiquement jusqu'à la fin du message
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent, // Défiler jusqu'à la fin
+      duration: const Duration(milliseconds: 300), // Durée du défilement
+      curve: Curves.easeInOut, // Courbe d'animation
+    ).then((_) {
+      // Afficher le menu contextuel après le défilement
+      showDialog(
+        context: context,
+        barrierColor: Colors.transparent, // Fond transparent pour le flou
+        barrierDismissible: true, // Permettre la fermeture en cliquant en dehors
+        builder: (BuildContext context) {
+          return GestureDetector(
+            onTap: () {
+              // Fermer le menu contextuel lorsque l'utilisateur clique en dehors
+              Navigator.pop(context);
+            },
+            child: Stack(
+              children: [
+                // Effet de flou sur l'arrière-plan
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                  child: Container(
+                    color: Colors.transparent,
                   ),
                 ),
-              ),
-              // Positionnement du menu contextuel en dessous du MessageBubble
-              Positioned(
-                top: offset.dy + renderBox.size.height, // Position en dessous du message
-                right: widget.isMe ? offset.dx : null, // Ajuster la position horizontale
-                left: widget.isMe ? null : offset.dx,
-                child: GestureDetector(
-                  onTap: () {
-                    // Empêcher la fermeture du menu lorsque l'utilisateur clique sur le menu
-                  },
+                // Positionnement du MessageBubble au-dessus du flou
+                Positioned(
+                  top: offset.dy,
+                  left: widget.isMe ? null : offset.dx, // Ajuster la position horizontale
+                  right: widget.isMe ? offset.dx : null,
                   child: Material(
                     color: Colors.transparent,
-                    child: Container(
-                      width: 160, // Largeur fixe pour le menu
-                      decoration: BoxDecoration(
-                        color: AppColors.backgroundColor.withOpacity(0.9),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            blurRadius: 10,
-                            spreadRadius: 2,
-                            offset: const Offset(0, 4),
+                    child: Column(
+                      crossAxisAlignment:
+                          widget.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                      children: [
+                        // Bulle de message
+                        BubbleSpecialThree(
+                          text: widget.text,
+                          isSender: widget.isMe,
+                          color: widget.isMe ? AppColors.primaryColor : Colors.grey[800]!,
+                          tail: true,
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
                           ),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          // Option pour répondre
-                          _buildMenuOption(
-                            icon: Icons.reply,
-                            label: "Répondre",
-                            onTap: () {
-                              Navigator.pop(context); // Fermer le menu
-                              widget.onReply(); // Appeler la fonction de réponse
-                            },
+                          sent: true,
+                          seen: true,
+                        ),
+                        // Horodatage
+                        Padding(
+                          padding: const EdgeInsets.only(top: 4),
+                          child: Text(
+                            widget.time,
+                            style: TextStyle(
+                              color: Colors.white.withOpacity(0.7),
+                              fontSize: 10,
+                            ),
                           ),
-                          // Option pour copier
-                          _buildMenuOption(
-                            icon: Icons.copy,
-                            label: "Copier",
-                            onTap: () {
-                              Navigator.pop(context); // Fermer le menu
-                              widget.onCopy(); // Appeler la fonction de copie
-                            },
-                          ),
-                          // Option pour épingler
-                          _buildMenuOption(
-                            icon: Icons.push_pin,
-                            label: "Épingler",
-                            onTap: () {
-                              Navigator.pop(context); // Fermer le menu
-                              widget.onPin(); // Appeler la fonction d'épinglage
-                            },
-                          ),
-                          // Option pour transférer
-                          _buildMenuOption(
-                            icon: Icons.forward,
-                            label: "Transférer",
-                            onTap: () {
-                              Navigator.pop(context); // Fermer le menu
-                              widget.onForward(); // Appeler la fonction de transfert
-                            },
-                          ),
-                          // Option pour supprimer
-                          _buildMenuOption(
-                            icon: Icons.delete,
-                            label: "Supprimer",
-                            onTap: () {
-                              Navigator.pop(context); // Fermer le menu
-                              widget.onDelete(); // Appeler la fonction de suppression
-                            },
-                          ),
-                          // Option pour sélectionner
-                          _buildMenuOption(
-                            icon: Icons.check_box,
-                            label: "Sélectionner",
-                            onTap: () {
-                              Navigator.pop(context); // Fermer le menu
-                              widget.onSelect(); // Appeler la fonction de sélection
-                            },
-                          ),
-                        ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                // Positionnement du menu contextuel
+                Positioned(
+                  top: showBelow ? offset.dy + renderBox.size.height : offset.dy - 160, // Ajuster la position verticale
+                  right: widget.isMe ? offset.dx : null, // Ajuster la position horizontale
+                  left: widget.isMe ? null : offset.dx,
+                  child: GestureDetector(
+                    onTap: () {
+                      // Empêcher la fermeture du menu lorsque l'utilisateur clique sur le menu
+                    },
+                    child: Material(
+                      color: Colors.transparent,
+                      child: Container(
+                        width: 160, // Largeur fixe pour le menu
+                        decoration: BoxDecoration(
+                          color: AppColors.backgroundColor.withOpacity(0.9),
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),
+                              blurRadius: 10,
+                              spreadRadius: 2,
+                              offset: const Offset(0, 4),
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            _buildMenuOption(
+                              icon: Icons.copy,
+                              label: "Copier",
+                              onTap: () {
+                                Navigator.pop(context); // Fermer le menu
+                                widget.onCopy(); // Appeler la fonction de copie
+                              },
+                            ),
+                            // Option pour épingler
+                            _buildMenuOption(
+                              icon: Icons.push_pin,
+                              label: "Épingler",
+                              onTap: () {
+                                Navigator.pop(context); // Fermer le menu
+                                widget.onPin(); // Appeler la fonction d'épinglage
+                              },
+                            ),
+                            // Option pour transférer
+                            _buildMenuOption(
+                              icon: Icons.forward,
+                              label: "Transférer",
+                              onTap: () {
+                                Navigator.pop(context); // Fermer le menu
+                                widget.onForward(); // Appeler la fonction de transfert
+                              },
+                            ),
+                            // Option pour supprimer
+                            _buildMenuOption(
+                              icon: Icons.delete,
+                              label: "Supprimer",
+                              onTap: () {
+                                Navigator.pop(context); // Fermer le menu
+                                widget.onDelete(); // Appeler la fonction de suppression
+                              },
+                            ),
+                            // Option pour sélectionner
+                         
+                          ],
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
+              ],
+            ),
+          );
+        },
+      );
+    });
   }
 
   // Méthode pour construire une option du menu
@@ -231,36 +227,39 @@ class _MessageBubbleState extends State<MessageBubble> {
         alignment: widget.isMe ? Alignment.centerRight : Alignment.centerLeft,
         child: GestureDetector(
           onLongPress: () => _showContextMenu(context), // Détecter un appui long
-          child: Column(
-            crossAxisAlignment:
-                widget.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Bulle de message
-              BubbleSpecialThree(
-                text: widget.text,
-                isSender: widget.isMe,
-                color: widget.isMe ? AppColors.primaryColor : Colors.grey[800]!,
-                tail: true,
-                textStyle: const TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
+          child: SingleChildScrollView(
+            controller: _scrollController, // Utiliser le contrôleur de défilement
+            child: Column(
+              crossAxisAlignment:
+                  widget.isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                // Bulle de message
+                BubbleSpecialThree(
+                  text: widget.text,
+                  isSender: widget.isMe,
+                  color: widget.isMe ? AppColors.primaryColor : Colors.grey[800]!,
+                  tail: true,
+                  textStyle: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                  sent: true,
+                  seen: true,
                 ),
-                sent: true,
-                seen: true,
-              ),
-              // Horodatage
-              Padding(
-                padding: const EdgeInsets.only(top: 4),
-                child: Text(
-                  widget.time,
-                  style: TextStyle(
-                    color: Colors.white.withOpacity(0.7),
-                    fontSize: 10,
+                // Horodatage
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Text(
+                    widget.time,
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.7),
+                      fontSize: 10,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
